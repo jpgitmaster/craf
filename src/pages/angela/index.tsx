e7edda677673b7716b7ca959f024f81ac9813ff6
jpvasquez <jlvasquez@dnl.com.ph>
2024-03-16 13:53:06 +0800

    updated

--- src/pages/angela/index.tsx
+++ src/pages/angela/index.tsx
@@ -3,54 +3,56 @@ import Image from 'next/image'
 import styles from './../Home.module.scss'
 export default function Home() {
   return (
-    <div className={styles.bizCard}>
-      <div className={styles.bizCardTop}>
-        <div className={styles.bizCardTopLeft}>
-          <h1>
-            Angela Unida
-          </h1>
-          <p>
-            Sr Technica Project Manager
-          </p>
+    <div className={styles.bizCardBody}>
+      <div className={styles.bizCard}>
+        <div className={styles.bizCardTop}>
+          <div className={styles.bizCardTopLeft}>
+            <h1>
+              Angela Unida
+            </h1>
+            <p>
+              Sr Technica Project Manager
+            </p>
+          </div>
+          <div className={styles.bizCardTopRight}>
+            <Image src='/logo.png' alt='CRAF Logo' priority width={20} height={20} unoptimized={true} />
+          </div>
         </div>
-        <div className={styles.bizCardTopRight}>
-          <Image src='/logo.png' alt='CRAF Logo' priority width={20} height={20} unoptimized={true} />
-        </div>
-      </div>
-      <div className={styles.bizCardDetails}>
-        <div className={styles.bizCardDetailsLeft}>
-          <ul>
-            <li>
-              <div>
-                <Image src='/svgs/location.svg' alt='Location' priority width={20} height={20} unoptimized={true} />
-              </div>
-              <p>
-                <span>Location</span>
-                7 Metroville 2, Tandang Sora Ave, Project 8, Quezon City, Metro Manila, Philippines, 1116
-              </p>
-            </li>
-            <li>
-              <div>
-                <Image src='/svgs/phone.svg' alt='Phone' priority width={20} height={20} unoptimized={true} />
-              </div>
-              <p>
-                <span>Mobile Number</span>
-                +63 927 776 1819
-              </p>
-            </li>
-            <li>
-              <div>
-                <Image src='/svgs/email.svg' alt='Email' priority width={20} height={20} unoptimized={true} />
-              </div>
-              <p>
-                <span>Email Address</span>
-                rflorante@gmail.com
-              </p>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.bizCardDetailsRight}>
-          <QRCodeSVG value="https://reactjs.org/" width={100} />
+        <div className={styles.bizCardDetails}>
+          <div className={styles.bizCardDetailsLeft}>
+            <ul>
+              <li>
+                <div>
+                  <Image src='/svgs/location.svg' alt='Location' priority width={20} height={20} unoptimized={true} />
+                </div>
+                <p>
+                  <span>Location</span>
+                  7 Metroville 2, Tandang Sora Ave, Project 8, Quezon City, Metro Manila, Philippines, 1116
+                </p>
+              </li>
+              <li>
+                <div>
+                  <Image src='/svgs/phone.svg' alt='Phone' priority width={20} height={20} unoptimized={true} />
+                </div>
+                <p>
+                  <span>Mobile Number</span>
+                  +63 927 776 1819
+                </p>
+              </li>
+              <li>
+                <div>
+                  <Image src='/svgs/email.svg' alt='Email' priority width={20} height={20} unoptimized={true} />
+                </div>
+                <p>
+                  <span>Email Address</span>
+                  rflorante@gmail.com
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.bizCardDetailsRight}>
+            <QRCodeSVG value="https://reactjs.org/" width={100} />
+          </div>
         </div>
       </div>
     </div>
