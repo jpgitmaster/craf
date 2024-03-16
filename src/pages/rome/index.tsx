a213eb73ca823b13f0b8b1fb76c4b624b1d741f3
jpvasquez <jlvasquez@dnl.com.ph>
2024-03-16 14:39:58 +0800

    updated

--- src/pages/rome/index.tsx
+++ src/pages/rome/index.tsx
@@ -51,7 +51,7 @@ export default function Home() {
             </ul>
           </div>
           <div className={styles.bizCardDetailsRight}>
-            <QRCodeSVG value="https://reactjs.org/" width={80} />
+            <QRCodeSVG value="https://reactjs.org/" width={75} />
           </div>
         </div>
       </div>
